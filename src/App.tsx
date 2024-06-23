6ef442ce811a355e2ab240a66499cb3f2ae98a21
cristianchaparroa <cristianchaparroa@gmail.com>
2024-06-22 23:42:32 -0500

    use twa

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,16 @@
-import { useState } from "react";
-import { Button } from "./components/ui/button";
+import WebApp from '@twa-dev/sdk'
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  function handleCountUpdate() {
-    setCount((prev) => prev + 1);
-  }
 
   return (
     <>
-      <main className="container mx-auto p-8">
-        <p className="text-3xl">
-          This is a simple Vite React Typescript starterpack
-        </p>
-        <Button onClick={handleCountUpdate}>Inc Count: {count}</Button>
-      </main>
+      <div>
+        <button onClick={() => WebApp.showAlert(`Hello World!`)}>
+            Show Alert
+        </button>
+      </div>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
